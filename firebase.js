61752cf50c7c971d931d9d3e25c0c685995aa92d
florenciasueiro <florencia.sueiro@davinci.edu.ar>
2024-10-09 22:23:16 -0300

    Añade firebase creada

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyCGnVtK5Wze8oLNW4RIe0ENZgtHMa3J7ZU",
+  authDomain: "paez-sueiro-cwm-parcial1.firebaseapp.com",
+  projectId: "paez-sueiro-cwm-parcial1",
+  storageBucket: "paez-sueiro-cwm-parcial1.appspot.com",
+  messagingSenderId: "14180494162",
+  appId: "1:14180494162:web:f5a22bd175ae02e565688b"
 };
 
 firebase.initializeApp(firebaseConfig);
